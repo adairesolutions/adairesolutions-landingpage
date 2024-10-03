3440b89b3eddeca17a3edb97d9b9769dc30a547d
Gerald Zandisko <geraldzandisko@burgeonadaire.com>
2024-10-03 16:40:28 +0700

    add firebase functions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,20 @@
 import type { Metadata } from "next";
+import '../services/firebase';
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./responsives.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "The Adaire Solutions",
   description: "The Adaire Solutions",
+  authors: [
+    {
+      name: "The Adaire Solutions",
+      url: "https://adairesolutions.com"
+    }
+  ]
 };
 
 export default function RootLayout({
